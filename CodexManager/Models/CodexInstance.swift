facb53039cd68540591f7774a62ec86eaa16f8d7
Trong Nguyen <nguyenphutrong.dev@gmail.com>
2026-06-25 20:46:59 +0700

    feat: extend instances with launch options

--- CodexManager/Models/CodexInstance.swift
+++ CodexManager/Models/CodexInstance.swift
@@ -5,6 +5,8 @@ struct CodexInstance: Identifiable, Codable, Equatable {
     var name: String
     var iconPath: String?
     var codexHome: String
+    var extraEnvVars: [String: String]
+    var launchArgs: [String]
     var createdAt: Date
     var lastLaunchedAt: Date?
 
@@ -13,6 +15,8 @@ struct CodexInstance: Identifiable, Codable, Equatable {
         name: String,
         iconPath: String? = nil,
         codexHome: String,
+        extraEnvVars: [String: String] = [:],
+        launchArgs: [String] = [],
         createdAt: Date = Date(),
         lastLaunchedAt: Date? = nil
     ) {
@@ -20,8 +24,47 @@ struct CodexInstance: Identifiable, Codable, Equatable {
         self.name = name
         self.iconPath = iconPath
         self.codexHome = codexHome
+        self.extraEnvVars = extraEnvVars
+        self.launchArgs = launchArgs
         self.createdAt = createdAt
         self.lastLaunchedAt = lastLaunchedAt
+    }
+}
+
+extension CodexInstance {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case iconPath
+        case codexHome
+        case extraEnvVars
+        case launchArgs
+        case createdAt
+        case lastLaunchedAt
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        iconPath = try container.decodeIfPresent(String.self, forKey: .iconPath)
+        codexHome = try container.decode(String.self, forKey: .codexHome)
+        extraEnvVars = try container.decodeIfPresent([String: String].self, forKey: .extraEnvVars) ?? [:]
+        launchArgs = try container.decodeIfPresent([String].self, forKey: .launchArgs) ?? []
+        createdAt = try container.decode(Date.self, forKey: .createdAt)
+        lastLaunchedAt = try container.decodeIfPresent(Date.self, forKey: .lastLaunchedAt)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encodeIfPresent(iconPath, forKey: .iconPath)
+        try container.encode(codexHome, forKey: .codexHome)
+        try container.encode(extraEnvVars, forKey: .extraEnvVars)
+        try container.encode(launchArgs, forKey: .launchArgs)
+        try container.encode(createdAt, forKey: .createdAt)
+        try container.encodeIfPresent(lastLaunchedAt, forKey: .lastLaunchedAt)
     }
 }
 
